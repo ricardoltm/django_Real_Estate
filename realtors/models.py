8d6da24a1a72ca81c472f58e038500fbab92654a
Ricardo Manso <ricardoltm@gmail.com>
2020-04-11 19:37:18 -0300

    Fixing realtors model field hire_date

--- realtors/models.py
+++ realtors/models.py
@@ -8,7 +8,7 @@ class Realtor(models.Model):
   phone = models.CharField(max_length=20)
   email = models.EmailField(max_length=50)
   is_mvp = models.BooleanField(default=False)
-  hire_date = models.DateTimeField(dafault=datetime.now, blank=True)
+  hire_date = models.DateTimeField(default=datetime.now, blank=True)
 
   def __str__(self):
     return self.name
